3666d6f19b559fc9e6d68be67b4020cbf4d99cc2
Joaquin <joaquin.quezada2017@alu.uct.cl>
2019-09-03 20:41:18 -0400

    updated

--- Test2/analisis.py
+++ Test2/analisis.py
@@ -1,7 +1,7 @@
 from collections import Counter
 import spacy
 #numero de veces que se repite cada palabra
-text = "voy a dormir, estoy escribiendo algo, el diputado le pasa la palabra al diputado"
+text = "voy a dormir, estoy escribiendo algo, el diputado le pasa la palabra al diputado, habla Roberto"
 print Counter(text.split(" "))
 nlp = spacy.load("es_core_news_sm")
 doc = nlp(text.decode('utf8'))
@@ -9,4 +9,7 @@ print "frases:",[chunk.text for  chunk  in doc.noun_chunks]
 print "verbos:", [token.lemma_ for token in doc if token.pos_ == "VERB" ]
 for token in doc:
     print token.pos_,token.text,token.lemma_
-print [token.text for token in doc if token.pos_ != "AUX" and token.pos_ != "ADP" and token.pos_ != "PUNCT" and token.pos_ != "DET" and token.pos_ != "PRON" ]
+print [token.text for token in doc if token.pos_ == "NOUN" ]
+print "Nombres:"
+for ent in doc.ents:
+    print ent.text
