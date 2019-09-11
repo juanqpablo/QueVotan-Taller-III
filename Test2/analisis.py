f9d23cd6d4cea2c5c7cec47fd7c6ef8db5407e6c
Joaquin <joaquin.quezada2017@alu.uct.cl>
2019-09-11 20:55:52 -0300

    pequeños cambios

--- Test2/analisis.py
+++ Test2/analisis.py
@@ -1,10 +1,14 @@
+# -*- coding: utf-8 -*-
 from collections import Counter
 import spacy
+import sys
 #numero de veces que se repite cada palabra
 text = "voy a dormir, estoy escribiendo algo, el diputado le pasa la palabra al diputado, habla Roberto"
+msg = "Modifica la ley N°18.892, General de Pesca y Acuicultura, en materia de prohibición de captura de especies salmonídeas provenientes de cultivos de acuicultura"
+print msg
 print Counter(text.split(" "))
 nlp = spacy.load("es_core_news_sm")
-doc = nlp(text.decode('utf8'))
+doc = nlp(msg.decode('utf8'))
 print "frases:",[chunk.text for  chunk  in doc.noun_chunks]
 print "verbos:", [token.lemma_ for token in doc if token.pos_ == "VERB" ]
 for token in doc:
